8fff3e83216b42a9283371cdbdd588986d76c0c4
grantposell <grantposell@gmail.com>
2018-04-04 23:27:49 -0500

    liri app ready

--- liri.js
+++ liri.js
@@ -21,8 +21,9 @@ var inputValue = "";
 for (var i = 3; i < argument.length; i++) {
     inputValue += argument[i] + " ";
 }
-
+//Adding a switch case to allow multiple calls to be made to pull different information
 switch (value) {
+    //calling the tweets from naughtxchargers twitter page
     case 'my-tweets':
         var params = { screen_name: 'naughtxchargers', count: 3 };
         client.get('statuses/user_timeline', params, function (error, tweets, response) {
@@ -34,28 +35,77 @@ switch (value) {
             }
         });
         break;
-    // case 'spotify-this-song':
-    //     if (songName === "") {
-    //         songName = "The Sign";
-    //     }
-    //     // spotify.search({ type: 'track', query: songName, limit: 5 }, function (err, data) {
-    //     //     if (err) {
-    //     //         return console.log('Error occurred: ' + err);
-    //     //     }
-    //     var title = process.argv[3];
+    //Calling songs using the spotify npm module    
+    case 'spotify-this-song':
+        var songTitle = process.argv[3];
+        var trackAssets;
+        if (songTitle) {
+            spotify.search({ type: 'track', query: songTitle, limit: 1 }, function (err, data) {
+                if (err) {
+                    return console.log('Error occurred: ' + err);
+                }
+                trackAssets = data.tracks.items[0];
+                console.log(`Song:${trackAssets.name}'\n' Artist: ${trackAssets.artists[0].name}'\n' Spotify Link: ${trackAssets.album.external_urls.spotify}'\n' + Album Name: ${trackAssets.album.name}`);
+            });
+        } else {
+            spotify.search({ type: 'track', query: "The Sign", limit: 1 }, function (err, data) {
+                if (err) {
+                    return console.log('Error occurred: ' + err);
+                }
+                trackAssets = data.tracks.items[0];
+                console.log(`Song:${trackAssets.name}'\n' Artist: ${trackAssets.artists[0].name}'\n' Spotify Link: ${trackAssets.album.external_urls.spotify}'\n' + Album Name: ${trackAssets.album.name}`);
+            });
+        };
+        break;
+    //Allowing calls for multiple word titles  
+    case 'movie-this':
+        var extendedArgs = process.argv;
+        var movieName = "";
+       
+        for (var i = 3; i < extendedArgs.length; i++) {
 
-    //     break;
-    // case 'movie-this':
+            if (i > 3 && i < extendedArgs.length) {
+                movieName = movieName + "+" + extendedArgs[i];
+            }
+            else {
+                movieName += extendedArgs[i];
+            }
+        }
+        //Using the API call, the user can get information on movies on IMDB
+        var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=87e2af0b";
+        if (extendedArgs[3]) {
+            request(queryUrl, function (error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    console.log("Title of the movie: " + JSON.parse(body).Title + '\n' + "Year the movie came out: " + JSON.parse(body).Year + '\n' + "IMDB Rating of the movie: " + JSON.parse(body).imdbRating + '\n' + "Rotten Tomatoes Rating of the movie: " + JSON.parse(body).Ratings[1].Value + '\n' + "Country where the movie was produced: " + JSON.parse(body).Production + '\n' + "Language of the movie: " + JSON.parse(body).Language + '\n' + "Plot of the movie: " + JSON.parse(body).Plot + '\n' + "Actors/Actresses in the movie: " + JSON.parse(body).Actors);
+                }
+            });
+        }
+        else {
+            movieName = "Mr. Nobody";
+            queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=87e2af0b";
+            request(queryUrl, function (error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    console.log("Title of the movie: " + JSON.parse(body).Title + '\n' + "Year the movie came out: " + JSON.parse(body).Year + '\n' + "IMDB Rating of the movie: " + JSON.parse(body).imdbRating + '\n' + "Rotten Tomatoes Rating of the movie: " + JSON.parse(body).Ratings[1].Value + '\n' + "Country where the movie was produced: " + JSON.parse(body).Production + '\n' + "Language of the movie: " + JSON.parse(body).Language + '\n' + "Plot of the movie: " + JSON.parse(body).Plot + '\n' + "Actors/Actresses in the movie: " + JSON.parse(body).Actors);
 
-    //     break;
-    // case "do-what-it-says":
-    //     fs.readFile("./random.txt", "utf8", function (err, data) {
-    //         if (err) {
-    //             return console.log(err);
-    //         }
-    //         var output = data.split(",");
-    //     });
-    //     break;
+                }
+            });
+        }
+        break;
+    case "do-what-it-says":
+        fs.readFile("./random.txt", "utf8", function (err, data) {
+            if (err) {
+                return console.log(err);
+            }
+            var output = data.split(",");
+            spotify.search({ type: 'track', query: output[1], limit: 1 }, function (err, data) {
+                if (err) {
+                    return console.log('Error occurred: ' + err);
+                }
+                trackItems = data.tracks.items[0];
+                console.log("Song: " + trackItems.name + '\n' + "Artist: " + trackItems.artists[0].name + '\n' + "Spotify Link: " + trackItems.album.external_urls.spotify + '\n' + "Album Name: " + trackItems.album.name);
+            });
+        });
+        break;
     default:
         console.log("Not a valid operation. Choose:my-tweets, spotify-this-song,  movie-this, do-what-it-says");
         break;
